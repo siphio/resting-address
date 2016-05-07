831b4a9bf8a44b93f29582981d0d450b262fa39f
siphio <joncorker@gmail.com>
2016-05-07 09:25:54 +0100

    streamline config and init
    modify __init__ to pull config only once and store in self.

--- DB.py
+++ DB.py
@@ -1,11 +1,12 @@
 import sqlite3, config
 
 class sqlite_noFTS:
-    def __init__(self, dbfile):
+    def __init__(self, dbfile=config.database_filename, max_results=config.max_results):
         self.conn = sqlite3.connect(dbfile)
+        self.max_results = max_results
 
     def feature_to_geojson(self, item):
-    	return {"type"      : "Feature",
+        return {"type"      : "Feature",
                 "properties": {
                     "uprn"      : item[0],
                     "address"   : item[1]
@@ -18,19 +19,20 @@ class sqlite_noFTS:
 
     def address_query(self, search_string):
         featureList = []
-        sql = "select uprn, address_label, latitude, longitude from vlookup where address_label like ?"
-        cursor = self.conn.execute( sql, ["%"+search_string+"%"] )
-        for row in cursor:
-        	featureList.append( self.feature_to_geojson(row) )
-        if (not config.max_results) or (len(featureList) < config.max_results):
-        	return featureList
+        if self.max_results:
+            sql = "select uprn, address_label, latitude, longitude from vlookup where address_label like ? limit ?"
+            cursor = self.conn.execute( sql, [ "%"+search_string+"%", str(self.max_results) ] )
         else:
-        	return featureList[0:config.max_results]
+            sql = "select uprn, address_label, latitude, longitude from vlookup where address_label like ?"
+            cursor = self.conn.execute( sql,  ["%"+search_string+"%"] )
+        for row in cursor:
+            featureList.append( self.feature_to_geojson(row) )
+        return featureList
 
 
 
 if __name__ == "__main__":
-    db = sqlite_noFTS(config.database_filename)
+    db = sqlite_noFTS()
     results = db.address_query("fish")
     import json
     print len( results )
