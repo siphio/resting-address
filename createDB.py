380f45e37240c96d355877ec3c0153102955c000
siphio <joncorker@gmail.com>
2016-05-15 00:55:45 +0100

    Delete createDB.py

--- createDB.py
+++
@@ -1,8 +0,0 @@
-import sqlite3, config
-
-sqlscript = "database_creation_sqlite.sql"
-
-con = sqlite3.connect(config.database_filename)
-
-with open(sqlscript, 'r') as sql:
-	con.executescript( sql.read() )
