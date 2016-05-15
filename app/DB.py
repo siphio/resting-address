0a6f7eb103ae5e86182e571f26832456f10b5b99
siphio <joncorker@gmail.com>
2016-05-15 12:34:12 +0100

    updated UPRN search

--- app/DB.py
+++ app/DB.py
@@ -46,7 +46,7 @@ class sqlite_noFTS:
             featureList.append( self.feature_to_geojson(row) )
         return self.featureList_wrapper( featureList )
         
-    def uprn_query(self, uprn):
+    def uprn_query_old(self, uprn):
         featureList = []
         sql = "select uprn, address_label, latitude, longitude from vlookup where uprn = ?"
         cursor = self.conn.execute( sql, [ str(uprn) ] )
@@ -55,6 +55,47 @@ class sqlite_noFTS:
         # a single feature returned is totally within geojson spec, but feature collection has scope for metadata.
         return self.featureList_wrapper( featureList )
 
+    def uprn_query(self, uprn):
+        sql = "select * from addressbase where uprn = ?"
+        cursor = self.conn.execute( sql, [ str(uprn) ] )
+        for row in cursor:
+        	 result = {
+        	 "type"      : "Feature",
+        	 "properties" : {
+        	 "uprn": row[0],
+        	 "os_address_toid": row[1],
+        	 "udprn": row[2],
+        	 "organisation_name": row[3],
+        	 "department_name": row[4],
+        	 "po_box_number" : row[5],
+        	 "sub_building_name": row[6],
+        	 "building_name": row[7],
+        	 "building_number": row[8],
+        	 "dependent_thoroughfare": row[9],
+        	 "thoroughfare": row[10],
+        	 "post_town": row[11],
+        	 "double_dependent_locality": row[12],
+        	 "dependent_locality": row[13],
+        	 "postcode": row[14],
+        	 "postcode_type": row[15],
+        	 "x": row[16],
+        	 "y": row[17],
+        	 "lat": row[18],
+        	 "long": row[19],
+        	 "rpc": row[20],
+        	 "country": row[21],
+        	 "change_type": row[22],
+        	 "la_start_date": row[23],
+        	 "rm_start_date": row[24],
+        	 "last_update": row[25],
+        	 "class": row[26],
+        	 },
+        	 "geometry": {
+        	 	"type" : "Point",
+        	 	"coordinates": [ row[19], row[18] ]
+        	 }
+        	 }
+        return self.featureList_wrapper( [ result ] )
 
 
 if __name__ == "__main__":
@@ -68,4 +109,4 @@ if __name__ == "__main__":
     import json
     with open('tests.js', 'w') as f:
         json.dump( db.address_query("council"), f )
-    print( json.dumps( db.uprn_query(10015253452) ))
+    print( json.dumps( db.uprn_query_2(10015253452) ))
